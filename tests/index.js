1b00c212463d11b2c8f99a96822d3b9c0bffb662
Steve Elliott <steve@tegud.net>
2016-03-26 14:24:02 +0000

    Info, Warn, Error levels

--- tests/index.js
+++ tests/index.js
@@ -34,19 +34,54 @@ describe('sends logged event to specified UDP port', function() {
         log.info({ message: 'test log message' });
     });
 
-    it('logs message with level', done => {
-        const log = new Logger(logEvents => logEvents
-            .addLevelToAdditionalFields()
-            .subscribe('udp', { host: 'localhost', port: 1234 }));
+    describe('logs message with level', () => {
+        it('info', done => {
+            const log = new Logger(logEvents => logEvents
+                .addLevelToAdditionalFields()
+                .subscribe('udp', { host: 'localhost', port: 1234 }));
 
-		bindToUdpAndHandleMessage(1234, parsedData => {
-    		parsedData.should.be.eql({
-    			message: 'test log message',
-                level: 'info'
-    		});
-            done();
+                bindToUdpAndHandleMessage(1234, parsedData => {
+                    parsedData.should.be.eql({
+                        message: 'test log message',
+                        level: 'info'
+                    });
+                    done();
+                });
+
+                log.info({ message: 'test log message', level: 'info' });
         });
 
-        log.info({ message: 'test log message', level: 'info' });
+        it('warn', done => {
+            const log = new Logger(logEvents => logEvents
+                .addLevelToAdditionalFields()
+                .subscribe('udp', { host: 'localhost', port: 1234 }));
+
+                bindToUdpAndHandleMessage(1234, parsedData => {
+                    parsedData.should.be.eql({
+                        message: 'test log message',
+                        level: 'warn'
+                    });
+                    done();
+                });
+
+                log.warn({ message: 'test log message', level: 'warn' });
+        });
+
+
+        it('error', done => {
+            const log = new Logger(logEvents => logEvents
+                .addLevelToAdditionalFields()
+                .subscribe('udp', { host: 'localhost', port: 1234 }));
+
+                bindToUdpAndHandleMessage(1234, parsedData => {
+                    parsedData.should.be.eql({
+                        message: 'test log message',
+                        level: 'error'
+                    });
+                    done();
+                });
+
+                log.error({ message: 'test log message', level: 'error' });
+        });
     });
 });
