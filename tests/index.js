17d2dcc1e6ae577eb1e8e5f6416a2ce2f88848ed
Steve Elliott <steve@tegud.net>
2016-03-26 12:03:00 +0000

    First test passing
    So you can log stuff to udp, literally what you tell it to

---
+++ tests/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const should = require('should');
+const Logger = require('../lib/');
+const dgram = require('dgram');
+
+function bindToUdpAndHandleMessage(port, messageHandler) {
+    var udpClient = dgram.createSocket("udp4");
+
+	udpClient.bind(1234);
+
+	udpClient.on("message", function messageReceived(msg) {
+		var data = msg.toString('utf-8');
+		var parsedData = JSON.parse(data);
+
+		messageHandler(parsedData);
+
+		udpClient.close();
+	});
+}
+
+describe('sends logged event to specified UDP port', function() {
+    it('logs message', done => {
+        const log = new Logger(logEvents => logEvents
+            .subscribe('udp', { host: 'localhost', port: 1234 }));
+
+		bindToUdpAndHandleMessage(1234, parsedData => {
+    		parsedData.should.be.eql({
+    			message: 'test log message'
+    		});
+            done();
+        });
+
+        log.info({ message: 'test log message' });
+    });
+});
